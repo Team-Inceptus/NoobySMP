1e0d82197a36aab6bc9862dd61c8124317891cd4
GamerCoder215 <gamercoder215@gmail.com>
2022-02-21 15:51:03 -0600

    Update TitanBiome.java
    - Create Biomes
    - Map to replace vanilla biomes inside Titan World to these
    - Fixed updateChunksForAll() (thanks forums)

--- src/us/teaminceptus/noobysmp/generation/biomes/TitanBiome.java
+++ src/us/teaminceptus/noobysmp/generation/biomes/TitanBiome.java
@@ -1,24 +1,30 @@
 package us.teaminceptus.noobysmp.generation.biomes;
 
 import java.lang.reflect.Field;
+import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Color;
 import org.bukkit.Location;
+import org.bukkit.block.Biome;
+import org.bukkit.craftbukkit.v1_18_R1.CraftChunk;
 import org.bukkit.craftbukkit.v1_18_R1.CraftServer;
 import org.bukkit.craftbukkit.v1_18_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.google.common.collect.ImmutableMap;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.core.WritableRegistry;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkWithLightPacket;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.dedicated.DedicatedServer;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biome.BiomeBuilder;
 import net.minecraft.world.level.biome.BiomeGenerationSettings;
 import net.minecraft.world.level.biome.BiomeSpecialEffects;
@@ -28,7 +34,41 @@ import us.teaminceptus.noobysmp.SMP;
 
 public enum TitanBiome {
 
-	// TODO create titan biomes
+	// null in waterColor, skyColor, fogColor will set to default
+	// Use Null in last arg for foliage same as grass
+	
+	WITHERED_PLAINS("Withered Plains", false, "6b7077", "1e1e1e", null, "3d3d3d", null),
+	WITHERED_SWAMP("Withered Swamp", false, "5e5846", "1e1e1e", null, "2d2d2d", null),
+	WITHERED_BEACH("Withered Beach", false, "797f87", "1e1e1e", null, "4d4d4d", null),
+	WITHERED_FIELDS("Withered Fields", false, "6b7077", "1e1e1e", null, "2b2b2b", null),
+	
+	WITHERED_FOREST("Withered Forest", false, "6b7077", "1e1e1e", null, "4d4d4d", null),
+	BLOSSOM_FOREST("Blossom Forest", false, null, null, null, "f405fc", null),
+	COLD_BLOSSOM_FOREST("Cold Blossom Forest", false, null, null, null, "6a00ce", null),
+	STEAMING_BLOSSOM_FOREST("Steaming Blossom Forest", false, null, null, null, "ce0060", null),
+	NAVY_FOREST("Navy Forest", false, null, null, null, "000acc", "2a35fc"),
+	
+	LIME_PEAKS("Lime Peaks", false, null, null, null, "c1fc2a", null),
+	FROZEN_LIME_PEAKS("Frozen Lime Peaks", false, null, null, null, "2afc58", null),
+	
+	BLUE_SAVANNA("Blue Savanna", false, "96f1ff", null, null, "03a8c1", null),
+	STEAMING_BLUE_SAVANNA("Steaming Blue Savanna", false, "75ecff", null, null, "048ea3", null),
+	
+	STEAMING_JUNGLE("Steaming Jungle", false, null, null, "a1adfc", "e80909", "ff7f7f"),
+	BOILING_JUNGLE("Boiling Jungle", false, null, null, "8998f9", "d60a0a", "fc6a6a"),
+	
+	BOILING_MUSHROOM_FIELDS("Boiling Mushroom Fields", false, "020475", "ffbfd8", null, "f92c7e", null),
+	
+	YELLOW_TUNDRA("Yellow Tundra", true, "fff247", "fcf69c", null, "d8ca06", "fff23f"), 
+	
+	PINK_DESERT("Pink Desert", false, "ffa5de", null, null, "ff8cd4", null),
+	PURPLE_DESERT("Purple Desert", false, "c2a5ff", null, null, "da8cff", null),
+	
+	GREEN_RIVER("Green River", false, "a0f7c0", null, "bffcd6", null, null),
+	GREEN_OCEAN("Green Ocean", false, "a0f7c0", null, "bffcd6", null, null),
+	STEAMING_GREEN_OCEAN("Steaming Green Ocean", false, "02d64f", null, "bffcd6", null, null),
+	COLD_GREEN_OCEAN("Cold Green Ocean", false, "bcffd5", null, "bffcd6", null, null),
+	FROZEN_GREEN_OCEAN("Frozen Green Ocean", true, "d3ffe3", null, "bffcd6", null, null),
 	
 	;
 	
@@ -41,19 +81,86 @@ public enum TitanBiome {
 	private final String grassColor;
 	private final String foliageColor;
 
-	private Biome nmsBiome;
-	private ResourceKey<Biome> resourceKey;
+	private net.minecraft.world.level.biome.Biome nmsBiome;
+	private ResourceKey<net.minecraft.world.level.biome.Biome> resourceKey;
 	
 	private TitanBiome(String name, boolean snow, String waterColor, String fogColor, String skyColor, String grassColor, String foliageColor) {
 		this.name = name;
 		this.frozen = snow;
-		this.waterColor = waterColor;
-		this.fogColor = fogColor;
-		this.skyColor = skyColor;
+		this.waterColor = (waterColor == null ? "3F76E4" : waterColor);
+		this.fogColor = (fogColor == null ? "C0D8FF" : fogColor);
+		this.skyColor = (skyColor == null ? "78A7FF" : skyColor);
 		this.grassColor = grassColor;
-		this.foliageColor = foliageColor;
+		this.foliageColor = (foliageColor == null ? grassColor : foliageColor);
 	}
-
+	
+	public static final Map<TitanBiome, Biome[]> REPLACEABLES = ImmutableMap.<TitanBiome, Biome[]>builder()
+			.put(TitanBiome.WITHERED_PLAINS, new Biome[] {
+				Biome.PLAINS,
+				Biome.SUNFLOWER_PLAINS,
+			})
+			.put(TitanBiome.WITHERED_SWAMP, new Biome[] { Biome.SWAMP})
+			.put(TitanBiome.WITHERED_BEACH, new Biome[] {
+				Biome.BEACH,
+				Biome.SNOWY_BEACH
+			})
+			.put(TitanBiome.WITHERED_FOREST, new Biome[] { Biome.BIRCH_FOREST})
+			.put(TitanBiome.BLOSSOM_FOREST, new Biome[] { Biome.FOREST })
+			.put(TitanBiome.COLD_BLOSSOM_FOREST, new Biome[] { Biome.WINDSWEPT_FOREST })
+			.put(TitanBiome.STEAMING_BLOSSOM_FOREST, new Biome[] { Biome.FLOWER_FOREST })
+			.put(TitanBiome.NAVY_FOREST, new Biome[] { Biome.DARK_FOREST })
+			.put(TitanBiome.LIME_PEAKS, new Biome[] {
+				Biome.STONY_PEAKS,
+				Biome.JAGGED_PEAKS
+			})
+			.put(TitanBiome.FROZEN_LIME_PEAKS, new Biome[] {
+					Biome.FROZEN_PEAKS,
+					Biome.SNOWY_SLOPES
+			})
+			.put(TitanBiome.BLUE_SAVANNA, new Biome[] {
+				Biome.SAVANNA,
+				Biome.SAVANNA_PLATEAU,
+			})
+			.put(TitanBiome.STEAMING_BLUE_SAVANNA, new Biome[] {
+				Biome.WINDSWEPT_SAVANNA
+			})
+			.put(TitanBiome.STEAMING_JUNGLE, new Biome[] {
+				Biome.JUNGLE,
+				Biome.BAMBOO_JUNGLE
+			})
+			.put(TitanBiome.BOILING_JUNGLE, new Biome[] { Biome.SPARSE_JUNGLE })
+			.put(TitanBiome.BOILING_MUSHROOM_FIELDS, new Biome[] { Biome.MUSHROOM_FIELDS })
+			.put(TitanBiome.YELLOW_TUNDRA, new Biome[] { Biome.SNOWY_TAIGA })
+			.put(TitanBiome.PINK_DESERT, new Biome[] { Biome.DESERT })
+			.put(TitanBiome.PURPLE_DESERT, new Biome[] {
+				Biome.BADLANDS,
+				Biome.ERODED_BADLANDS,
+				Biome.WOODED_BADLANDS
+			})
+			.put(TitanBiome.GREEN_RIVER, new Biome[] {
+				Biome.RIVER,
+				Biome.FROZEN_RIVER
+			})
+			.put(TitanBiome.GREEN_OCEAN, new Biome[] {
+				Biome.OCEAN,
+				Biome.LUKEWARM_OCEAN,
+				Biome.DEEP_OCEAN
+			})
+			.put(TitanBiome.STEAMING_GREEN_OCEAN, new Biome[] {
+				Biome.DEEP_LUKEWARM_OCEAN,
+				Biome.WARM_OCEAN
+			})
+			.put(TitanBiome.COLD_GREEN_OCEAN, new Biome[] {
+				Biome.COLD_OCEAN,
+				Biome.DEEP_COLD_OCEAN
+			})
+			.put(TitanBiome.FROZEN_GREEN_OCEAN, new Biome[] {
+				Biome.DEEP_FROZEN_OCEAN,
+				Biome.FROZEN_OCEAN,
+			})
+			
+			.build();
+	
 	// Fetching Methods
 
 	public final boolean isFrozen() {
@@ -92,11 +199,11 @@ public enum TitanBiome {
 		return this.name;
 	}
 
-	public final Biome getNMSBiome() {
+	public final net.minecraft.world.level.biome.Biome getNMSBiome() {
 		return this.nmsBiome;
 	}
 
-	public final ResourceKey<Biome> getResourceKey() {
+	public final ResourceKey<net.minecraft.world.level.biome.Biome> getResourceKey() {
 		return this.resourceKey;
 	}
 
@@ -105,35 +212,35 @@ public enum TitanBiome {
 	public static void registerBiomes() throws Exception {
 		SMP plugin = JavaPlugin.getPlugin(SMP.class);
 
-		ResourceKey<Registry<Biome>> REGISTRY_KEY = Registry.BIOME_REGISTRY;
+		ResourceKey<Registry<net.minecraft.world.level.biome.Biome>> REGISTRY_KEY = Registry.BIOME_REGISTRY;
 		DedicatedServer server = ((CraftServer) Bukkit.getServer()).getServer();
 		
 		for (TitanBiome biome : values()) { 
-			ResourceKey<Biome> key = ResourceKey.<Biome>create(REGISTRY_KEY, new ResourceLocation("noobysmp", biome.name.toLowerCase().replace(' ', '_')));
+			ResourceKey<net.minecraft.world.level.biome.Biome> key = ResourceKey.<net.minecraft.world.level.biome.Biome>create(REGISTRY_KEY, new ResourceLocation("noobysmp", biome.name.toLowerCase().replace(' ', '_')));
 			biome.resourceKey = key;
 	
-			ResourceKey<Biome> oldKey = ResourceKey.<Biome>create(REGISTRY_KEY, new ResourceLocation("minecraft", "forest"));
-			WritableRegistry<Biome> registrywritable = server.registryAccess().ownedRegistryOrThrow(REGISTRY_KEY);
-			Biome forestbiome = registrywritable.get(oldKey);
+			ResourceKey<net.minecraft.world.level.biome.Biome> oldKey = ResourceKey.<net.minecraft.world.level.biome.Biome>create(REGISTRY_KEY, new ResourceLocation("minecraft", "forest"));
+			WritableRegistry<net.minecraft.world.level.biome.Biome> registrywritable = server.registryAccess().ownedRegistryOrThrow(REGISTRY_KEY);
+			net.minecraft.world.level.biome.Biome forestbiome = registrywritable.get(oldKey);
 			
-			BiomeBuilder builder = new Biome.BiomeBuilder();
+			BiomeBuilder builder = new net.minecraft.world.level.biome.Biome.BiomeBuilder();
 			builder.biomeCategory(forestbiome.getBiomeCategory());
 			builder.precipitation(forestbiome.getPrecipitation());
 			
-			Field biomeSettingMobsField = Biome.class.getDeclaredField("mobSettings");
+			Field biomeSettingMobsField = net.minecraft.world.level.biome.Biome.class.getDeclaredField("mobSettings");
 			biomeSettingMobsField.setAccessible(true);
 			MobSpawnSettings biomeSettingMobs = (MobSpawnSettings) biomeSettingMobsField.get(forestbiome);
 			builder.mobSpawnSettings(biomeSettingMobs);	
 			
-			Field biomeSettingGenField = Biome.class.getDeclaredField("generationSettings");
+			Field biomeSettingGenField = net.minecraft.world.level.biome.Biome.class.getDeclaredField("generationSettings");
 			biomeSettingGenField.setAccessible(true);
 			BiomeGenerationSettings biomeSettingGen = (BiomeGenerationSettings) biomeSettingGenField.get(forestbiome);
 			builder.generationSettings(biomeSettingGen);
 	
 			builder.downfall(0.8F);
 			builder.temperature(0.7F);
-			if (biome.frozen) builder.temperatureAdjustment(Biome.TemperatureModifier.FROZEN);
-			else builder.temperatureAdjustment(Biome.TemperatureModifier.NONE);
+			if (biome.frozen) builder.temperatureAdjustment(net.minecraft.world.level.biome.Biome.TemperatureModifier.FROZEN);
+			else builder.temperatureAdjustment(net.minecraft.world.level.biome.Biome.TemperatureModifier.NONE);
 			
 			BiomeSpecialEffects.Builder effectbuilder = new BiomeSpecialEffects.Builder();
 			effectbuilder.grassColorModifier(GrassColorModifier.NONE);
@@ -181,7 +288,7 @@ public enum TitanBiome {
 		biome.setBiome(loc);
 	}
 
-	private void setBiome(int x, int y, int z, Level w, Biome biome) {
+	private void setBiome(int x, int y, int z, Level w, net.minecraft.world.level.biome.Biome biome) {
 		BlockPos pos = new BlockPos(x, 0, z);
 
 		if (w.isLoaded(pos)) {
@@ -193,13 +300,13 @@ public enum TitanBiome {
 	}
 
 	private static void updateChunksForAll(Chunk chunk) {
-		// net.minecraft.world.level.chunk.LevelChunk c = ((CraftChunk)chunk).getHandle();
+		net.minecraft.world.level.chunk.LevelChunk c = ((CraftChunk)chunk).getHandle();
 		for (Player player : chunk.getWorld().getPlayers()) {
-				if (player.isOnline()) {
-						if((player.getLocation().distance(chunk.getBlock(0, 0, 0).getLocation()) < (Bukkit.getServer().getViewDistance() * 16))) {
-								// FIXME why ((CraftPlayer) player).getHandle().connection.send(new ClientboundLevelChunkWithLightPacket(c, , null, null, true));
-						}
+			if (player.isOnline()) {
+				if((player.getLocation().distance(chunk.getBlock(0, 0, 0).getLocation()) < (Bukkit.getServer().getViewDistance() * 16))) {
+					((CraftPlayer) player).getHandle().connection.send(new ClientboundLevelChunkWithLightPacket(c, c.getLevel().getLightEngine(), null, null, true));
 				}
+			}
 		}
 	}
 	
