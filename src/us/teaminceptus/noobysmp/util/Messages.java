dcd36f7e152ddbd7ed074559d7c56a2dac3c9b39
GamerCoder215 <gamercoder215@gmail.com>
2022-02-21 15:45:01 -0600

    Update Messages.java
    Convert to Interface for easier constants

--- src/us/teaminceptus/noobysmp/util/Messages.java
+++ src/us/teaminceptus/noobysmp/util/Messages.java
@@ -2,14 +2,20 @@ package us.teaminceptus.noobysmp.util;
 
 import org.bukkit.ChatColor;
 
-public class Messages {
+/**
+ * Interface for auto static & public fields.
+ * 
+ * <strong>Not legal for Implementation!</strong>
+ */
+public interface Messages {
 
-	public static final String NO_PERMISSION_CMD = ChatColor.RED + "You do not have permission to use this command!";
-	public static final String TOO_LOW_LEVEL = ChatColor.RED + "You do not have a high enough level to use this!";
-	public static final String PLAYER_ONLY_CMD = ChatColor.RED + "You need to be a player to execute this command!";
+	String NO_PERMISSION_CMD = ChatColor.RED + "You do not have permission to use this command!";
+	String TOO_LOW_LEVEL = ChatColor.RED + "You do not have a high enough level to use this!";
+	String PLAYER_ONLY_CMD = ChatColor.RED + "You need to be a player to execute this command!";
 
 	// Arguments
-	public static final String ARGUMENT_PLAYER = ChatColor.RED + "Please provide a valid player.";
-	public static final String ARGUMENT_INT = ChatColor.RED + "PLease provide a valid integer.";
+	String ARGUMENT_PLAYER = ChatColor.RED + "Please provide a valid player.";
+	String ARGUMENT_INT = ChatColor.RED + "Please provide a valid integer.";
+	String ARGUMENT_ITEM = ChatColor.RED + "Please provide a valid item.";
 	
 }
