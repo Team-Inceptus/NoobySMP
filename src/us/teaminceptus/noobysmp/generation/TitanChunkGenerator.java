8c672f386076f73b55ddf06daed62d4847f17450
GamerCoder215 <gamercoder215@gmail.com>
2022-02-26 23:46:36 -0600

    Update TitanChunkGenerator.java
    Starter Class based on Tutorial

--- src/us/teaminceptus/noobysmp/generation/TitanChunkGenerator.java
+++ src/us/teaminceptus/noobysmp/generation/TitanChunkGenerator.java
@@ -1,5 +1,24 @@
 package us.teaminceptus.noobysmp.generation;
 
-public class TitanChunkGenerator {
+import java.util.Random;
 
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.generator.WorldInfo;
+
+import us.teaminceptus.noobysmp.SMP;
+
+public class TitanChunkGenerator extends ChunkGenerator {
+	
+	protected SMP plugin;
+	
+	public TitanChunkGenerator(SMP plugin) {
+		this.plugin = plugin;
+	}
+	
+	public void generateNoise(WorldInfo info, Random r, int chunkX, int chunkZ, ChunkData chunk) {
+		
+	}
+	
+	public boolean shouldGenerateStructures() { return false; }
+	
 }
