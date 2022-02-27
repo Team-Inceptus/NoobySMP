7cfbd10f392bedcc159bdee6b1fdc92cf8214f86
GamerCoder215 <gamercoder215@gmail.com>
2022-02-26 23:43:39 -0600

    Update ServerManager.java
    - Player List Header and Footer
    - Update Item Loop
    - Space between arrow in Chat

--- src/us/teaminceptus/noobysmp/player/ServerManager.java
+++ src/us/teaminceptus/noobysmp/player/ServerManager.java
@@ -9,9 +9,12 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import us.teaminceptus.noobysmp.SMP;
 import us.teaminceptus.noobysmp.commands.admin.Ranks;
+import us.teaminceptus.noobysmp.commands.admin.Ranks.RankData;
 import us.teaminceptus.noobysmp.util.PlayerConfig;
 import us.teaminceptus.noobysmp.util.inventoryholder.CancelHolder;
 
@@ -24,25 +27,47 @@ public class ServerManager implements Listener {
 		Bukkit.getPluginManager().registerEvents(this, plugin);
 		plugin.getLogger().info("Loaded Server Manager Listener");
 	}
+	
+	public static final String IP = "smp.teaminceptus.us";
+	
+	public static final String TOP_PLAYER_LIST = "\n&6Hello, &a<player>!\n\n&6Welcome to NoobySMP!\n  &6You are playing on &a" + IP + "  \n";
 
+	public static final String BOTTOM_PLAYER_LIST = "\n&6<count> / <total> Players Online\n";
+	
+	private static final String formatList(Player p, String msg) {
+		return ChatColor.translateAlternateColorCodes('&', msg.replace("<player>", p.getName()).replace("<count>", Integer.toString(p.getServer().getOnlinePlayers().size())).replace("<total>", Integer.toString(p.getServer().getMaxPlayers()) ));
+	}
+	
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		Player p = e.getPlayer();
+		plugin.loadFiles();
 		PlayerConfig config = new PlayerConfig(p);
 
-		if (config.isMember()) {
-			ChatColor namePrefix = (config.getLevel() < 10 ? ChatColor.GRAY : ChatColor.WHITE);
-			ChatColor levelPrefix = (config.getLevel() > 200 ? ChatColor.GOLD : Ranks.LEVEL_COLOR.get(config.getLevel()));
-			p.setDisplayName(levelPrefix + (config.getLevel() > 100 ? ChatColor.BOLD + "" : "") + "[" + config.getLevel() + "] " + ChatColor.RESET + namePrefix + p.getName());
-			p.setPlayerListName(levelPrefix + p.getName());
-		} else {
-			
+		if (config.isMember()) config.updateRank();
+		else {
+			RankData data = Ranks.RANK_MAP.get(config.getRank());
+			Ranks.setRank(p, data.getChat(), data.getTab());
 		}
+		
+		if (p.hasPlayedBefore()) {
+			e.setJoinMessage(p.getDisplayName() + ChatColor.GREEN + ", welcome back to NoobySMP!");
+		} else {
+			e.setJoinMessage(p.getDisplayName() + ChatColor.GREEN + ", welcome to NoobySMP!");
+		}
+		
+		p.setPlayerListHeaderFooter(formatList(p, TOP_PLAYER_LIST), formatList(p, BOTTOM_PLAYER_LIST));
+		
+		new BukkitRunnable() {
+			public void run() {
+				config.updateItemsInConfig();
+			}
+		}.runTaskLater(plugin, 3);
 	}
 
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent e) {
-		e.setFormat("%s " + ChatColor.GRAY + ">" + ChatColor.RESET + "%s");
+		e.setFormat("%s " + ChatColor.GRAY + ">" + ChatColor.RESET + " %s");
 	}
 
 	// Global Managers
@@ -51,7 +76,7 @@ public class ServerManager implements Listener {
 	public void onClick(InventoryClickEvent e) {
 		InventoryView view = e.getView();
 
-		if (view.getTopInventory().getHolder() instanceof CancelHolder) e.setCancelled(true);
+		if (view.getTopInventory().getHolder() instanceof CancelHolder && !(e.getClickedInventory() instanceof PlayerInventory)) e.setCancelled(true);
 	}
 	
 }
