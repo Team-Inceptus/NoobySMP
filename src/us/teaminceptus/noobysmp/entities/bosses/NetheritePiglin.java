db1397db187b27aaa8f3a3714b9d06025de4f940
GamerCoder215 <gamercoder215@gmail.com>
2022-02-26 23:40:17 -0600

    Create NetheritePiglin.java
    Netherite Piglin Boss

---
+++ src/us/teaminceptus/noobysmp/entities/bosses/NetheritePiglin.java
@@ -0,0 +1,66 @@
+package us.teaminceptus.noobysmp.entities.bosses;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Piglin;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.google.common.collect.ImmutableMap;
+
+import us.teaminceptus.noobysmp.entities.bosses.BossSetup.Description;
+import us.teaminceptus.noobysmp.entities.bosses.BossSetup.DisplayName;
+import us.teaminceptus.noobysmp.entities.bosses.BossSetup.Experience;
+import us.teaminceptus.noobysmp.entities.bosses.BossSetup.HP;
+import us.teaminceptus.noobysmp.entities.bosses.BossSetup.Icon;
+import us.teaminceptus.noobysmp.entities.bosses.BossSetup.SpawnCost;
+import us.teaminceptus.noobysmp.entities.bosses.BossSetup.Tier;
+import us.teaminceptus.noobysmp.entities.bosses.attacks.Attacks.CancelChance;
+import us.teaminceptus.noobysmp.entities.bosses.attacks.Attacks.MinionSpawn;
+import us.teaminceptus.noobysmp.entities.bosses.attacks.Attacks.Offensive;
+import us.teaminceptus.noobysmp.materials.SMPMaterial;
+
+@Tier(1)
+@Description({"A subspecies of piglins that discovered", "the riches the ancients left for them."})
+@SpawnCost("ancient_debris:2")
+@HP(350)
+@Icon(Material.NETHERITE_INGOT)
+@DisplayName(value = "Netherite Piglin", cc = ChatColor.GRAY)
+public class NetheritePiglin extends SMPBoss<Piglin> {
+	
+	@Experience(15)
+	@MinionSpawn(chance = 10, type = EntityType.PIGLIN_BRUTE)
+	@MinionSpawn(chance = 20, type = EntityType.PIGLIN)
+	@CancelChance(5)
+	public NetheritePiglin(Location loc) {
+		super(Piglin.class, loc, 350, "Netherite Piglin", ImmutableMap.<ItemStack, Integer>builder()
+				.put(new ItemStack(Material.NETHERITE_INGOT), 100)
+				.build(),
+				ImmutableMap.<Attribute, Double>builder()
+				.put(Attribute.GENERIC_ATTACK_DAMAGE, 10D)
+				.build());
+		
+		entity.setImmuneToZombification(true);
+		entity.getEquipment().setHelmet(new ItemStack(Material.NETHERITE_HELMET));
+		entity.getEquipment().setHelmetDropChance(1F);
+		
+		entity.getEquipment().setChestplate(SMPMaterial.SUPER_NETHERITE_CHESTPLATE.getItem());
+		entity.getEquipment().setChestplateDropChance(0.005F);
+		
+		entity.getEquipment().setLeggings(SMPMaterial.SUPER_NETHERITE_LEGGINGS.getItem());
+		entity.getEquipment().setLeggingsDropChance(0.05F);
+		
+		entity.getEquipment().setBoots(new ItemStack(Material.NETHERITE_BOOTS));
+		entity.getEquipment().setBootsDropChance(1F);
+	}
+	
+	@Offensive(chance = 20)
+	public void knockback(Player target) {
+		target.setVelocity(entity.getLocation().getDirection().multiply(r.nextInt(3) + 1));
+		target.sendMessage(NAMETAG + "Goodbye!");
+	}
+
+}
