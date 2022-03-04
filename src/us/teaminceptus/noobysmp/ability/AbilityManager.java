e9d66448aa635956e7ca02b75f355ce2548327a5
GamerCoder215 <gamercoder215@gmail.com>
2022-03-04 17:23:53 +0000

    Ability Updates & Additions

--- src/us/teaminceptus/noobysmp/ability/AbilityManager.java
+++ src/us/teaminceptus/noobysmp/ability/AbilityManager.java
@@ -1,19 +1,28 @@
 package us.teaminceptus.noobysmp.ability;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Enderman;
+import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 
 import us.teaminceptus.noobysmp.SMP;
 import us.teaminceptus.noobysmp.enchants.SMPEnchant;
 import us.teaminceptus.noobysmp.materials.AbilityItem;
+import us.teaminceptus.noobysmp.materials.SMPMaterial;
 
 public class AbilityManager implements Listener {
 
@@ -25,10 +34,50 @@ public class AbilityManager implements Listener {
 	}
 	
 	/*
+	 * Name-specific abilities (i.e. Ocassus) 
+	 */
+
+	@EventHandler
+	public void onShoot(EntityShootBowEvent e) {
+		if (!(e.getEntity() instanceof Player p)) return;
+		
+		e.getProjectile().setMetadata("bow", new FixedMetadataValue(plugin, e.getBow()));
+		
+	}
+
+	private final Random r = new Random();
+
+	@EventHandler
+	public void onHit(ProjectileHitEvent e) {
+		if (e.getHitEntity() == null) return;
+		if (!(e.getHitEntity() instanceof Mob mob)) return;
+		Projectile proj = e.getEntity();
+		if (!(proj.getShooter() instanceof Player p)) return;
+
+		try {
+			ItemStack bow = (ItemStack) proj.getMetadata("bow").stream().filter(m -> m.getOwningPlugin() instanceof SMP).toList().get(0).value();
+		
+			if (AbilityItem.getByItem(bow) != null) {
+				AbilityItem item = AbilityItem.getByItem(bow);
+
+				if (item.name().toLowerCase().startsWith("occasus") && mob instanceof Enderman) {
+					e.setCancelled(true);
+					proj.remove();
+					double multiplier = Double.parseDouble(item.name().substring(item.name().length() - 1));
+
+					mob.damage(multiplier * (100 * r.nextDouble()), p);
+				}
+			}
+		} catch (NullPointerException err) {
+			// do nothing
+		}
+	}
+
+	/*
 	 * Used for Armor (Consumer)
 	 */
 	@EventHandler
-	public void onDamage(EntityDamageEvent e) {
+	public void onDamageAbility(EntityDamageEvent e) {
 		if (!(e.getEntity() instanceof Player p)) return;
 		
 		for (ItemStack armor : p.getInventory().getArmorContents()) {
@@ -78,6 +127,28 @@ public class AbilityManager implements Listener {
 			}
 		}
 	
+	}
+
+	/*
+	 * Item-Specific Abilities
+	 */
+
+	@EventHandler
+	public void onDamage(EntityDamageEvent e) {
+		if (!(e.getEntity() instanceof Player p)) return;
+
+		if (p.getEquipment().getBoots() != null) {
+			ItemStack item = p.getEquipment().getBoots();
+
+			if (SMPMaterial.getByItem(item) != null) {
+				SMPMaterial m = SMPMaterial.getByItem(item);
+
+				if (m.name().toLowerCase().contains("withering_boots") && e.getCause() == DamageCause.WITHER) {
+					e.setCancelled(true);
+				}
+			}	
+
+		}
 	}
 
 }
--- src/us/teaminceptus/noobysmp/ability/SMPAbility.java
+++ src/us/teaminceptus/noobysmp/ability/SMPAbility.java
@@ -1,14 +1,25 @@
 package us.teaminceptus.noobysmp.ability;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Fireball;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.ShulkerBullet;
 import org.bukkit.event.block.Action;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import us.teaminceptus.noobysmp.SMP;
 import us.teaminceptus.noobysmp.enchants.SMPEnchant;
 import us.teaminceptus.noobysmp.materials.AbilityItem;
 
@@ -19,14 +30,20 @@ import us.teaminceptus.noobysmp.materials.AbilityItem;
 public enum SMPAbility {
 	
 	// Abilities for Regular Items
-	INFINIBALL("InfiniBall", RIGHT_CLICK(), AbilityItem.INFINIBALL, p -> {
+	INFINIBALL("InfiniBall", RIGHT_CLICK(), 0, AbilityItem.INFINIBALL, p -> {
 		Fireball fb = p.getWorld().spawn(p.getLocation(), Fireball.class);
 		
 		fb.setDirection(p.getLocation().getDirection());
 		p.playSound(p, Sound.ITEM_FIRECHARGE_USE, 3F, 1F);
 	}),
 	
-	
+	BULLET_WAND("Bullet Wand", RIGHT_CLICK(), 40, AbilityItem.BULLET_WAND, p -> {
+		ShulkerBullet b = p.getWorld().spawn(p.getLocation(), ShulkerBullet.class);
+
+		Entity target = p.getNearbyEntities(15, 15, 15).stream().filter(e -> e instanceof LivingEntity en && !(e.getUniqueId().equals(p.getUniqueId()))).toList().get(0);
+		if (target != null) b.setTarget(target);
+		else b.setTarget(null);
+	}),
 	
 	// Enchants
 	THUNDERING("Thundering", RIGHT_CLICK(), SMPEnchant.THUNDERING, (p, target) -> p.getWorld().strikeLightning(target.getLocation()))
@@ -49,38 +66,45 @@ public enum SMPAbility {
 	
 	private final String name;
 	private final Action[] actions;
+	private final long cooldown;
 	
 	private final AbilityItem item;
 	private final SMPEnchant enchantment;
+
+	public static final Map<SMPAbility, List<UUID>> cooldowns = new HashMap<>();
 	
 	private final Consumer<Player> output;
 	private final BiConsumer<Player, LivingEntity> bioutput;
 	
-	private SMPAbility(String name, Action[] action, AbilityItem item, Consumer<Player> output) {
+	private SMPAbility(String name, Action[] action, long cooldown, AbilityItem item, Consumer<Player> output) {
 		this.name = name;
 		this.actions = action;
 		this.item = item;
 		this.enchantment = null;
 		this.output = output;
+		this.cooldown = cooldown;
 		this.bioutput = null;
 	}
 	
-	private SMPAbility(String name, Action[] action, SMPEnchant enchantment, Consumer<Player> output) {
+	private SMPAbility(String name, Action[] action, long cooldown, SMPEnchant enchantment, Consumer<Player> output) {
 		this.name = name;
 		this.actions = action;
 		this.enchantment = enchantment;
 		this.item = null;
 		this.output = output;
+		this.cooldown = cooldown;
 		this.bioutput = null;
 	}
 	
-	private SMPAbility(String name, Action[] action, AbilityItem item, BiConsumer<Player, LivingEntity> output) {
+	private SMPAbility(String name, Action[] action, long cooldown, AbilityItem item, BiConsumer<Player, LivingEntity> output) {
 		this.name = name;
 		this.actions = action;
 		this.item = item;
 		this.enchantment = null;
 		this.output = null;
 		this.bioutput = output;
+
+		this.cooldown = cooldown;
 	}
 	
 	private SMPAbility(String name, Action[] action, SMPEnchant enchantment, BiConsumer<Player, LivingEntity> output) {
@@ -90,6 +114,11 @@ public enum SMPAbility {
 		this.item = null;
 		this.output = null;
 		this.bioutput = output;
+		this.cooldown = 0;
+	}
+
+	static {
+		for (SMPAbility a : values()) cooldowns.put(a, new ArrayList<>());
 	}
 	
 	public static final SMPAbility getByItem(AbilityItem item) {
@@ -162,7 +191,12 @@ public enum SMPAbility {
 	 * @param p Player to use
 	 */
 	public void init(Player p) {
+		if (cooldowns.get(this).contains(p.getUniqueId())) {
+			p.sendMessage(ChatColor.RED + "Please wait before using this again!");
+			return;
+		}
 		output.accept(p);
+		initCooldown(p);
 	}
 	
 	/**
@@ -172,5 +206,16 @@ public enum SMPAbility {
 	 */
 	public void init(Player p, LivingEntity target) {
 		bioutput.accept(p, target);
+	}
+
+	public void initCooldown(Player p) {
+		cooldowns.get(this).add(p.getUniqueId());
+		SMPAbility a = this;
+		new BukkitRunnable() {
+			public void run() {
+				cooldowns.get(a).remove(p.getUniqueId());
+				p.sendMessage(ChatColor.GREEN + "You can use " + ChatColor.GOLD + a.name + ChatColor.GREEN + " again!");
+			}
+		}.runTaskLater(JavaPlugin.getPlugin(SMP.class), this.cooldown);
 	}
 }
