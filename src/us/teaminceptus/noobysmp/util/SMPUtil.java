d677ee262092081ea0ce2505a803c79321433d34
GamerCoder215 <gamercoder215@gmail.com>
2022-03-11 19:33:21 +0000

    Utility Updates

--- src/us/teaminceptus/noobysmp/util/SMPUtil.java
+++ src/us/teaminceptus/noobysmp/util/SMPUtil.java
@@ -5,17 +5,90 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.UUID;
 
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
-
 import com.google.gson.Gson;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 
 public class SMPUtil {
 		
 	private final static HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
 	
+	// Constants
+	public static final int ANVIL_ITEM_SLOT = 0;
+	public static final int ANVIL_ADDITION_SLOT = 1; 
+
+	private static class ColorUtil {
+
+		private static Map<ChatColor, ColorSet<Integer, Integer, Integer>> colorMap = new HashMap<ChatColor, ColorSet<Integer, Integer, Integer>>();
+	
+		static {
+			colorMap.put(ChatColor.BLACK, new ColorSet<Integer, Integer, Integer>(0, 0, 0));
+			colorMap.put(ChatColor.DARK_BLUE, new ColorSet<Integer, Integer, Integer>(0, 0, 170));
+			colorMap.put(ChatColor.DARK_GREEN, new ColorSet<Integer, Integer, Integer>(0, 170, 0));
+			colorMap.put(ChatColor.DARK_AQUA, new ColorSet<Integer, Integer, Integer>(0, 170, 170));
+			colorMap.put(ChatColor.DARK_RED, new ColorSet<Integer, Integer, Integer>(170, 0, 0));
+			colorMap.put(ChatColor.DARK_PURPLE, new ColorSet<Integer, Integer, Integer>(170, 0, 170));
+			colorMap.put(ChatColor.GOLD, new ColorSet<Integer, Integer, Integer>(255, 170, 0));
+			colorMap.put(ChatColor.GRAY, new ColorSet<Integer, Integer, Integer>(170, 170, 170));
+			colorMap.put(ChatColor.DARK_GRAY, new ColorSet<Integer, Integer, Integer>(85, 85, 85));
+			colorMap.put(ChatColor.BLUE, new ColorSet<Integer, Integer, Integer>(85, 85, 255));
+			colorMap.put(ChatColor.GREEN, new ColorSet<Integer, Integer, Integer>(85, 255, 85));
+			colorMap.put(ChatColor.AQUA, new ColorSet<Integer, Integer, Integer>(85, 255, 255));
+			colorMap.put(ChatColor.RED, new ColorSet<Integer, Integer, Integer>(255, 85, 85));
+			colorMap.put(ChatColor.LIGHT_PURPLE, new ColorSet<Integer, Integer, Integer>(255, 85, 255));
+			colorMap.put(ChatColor.YELLOW, new ColorSet<Integer, Integer, Integer>(255, 255, 85));
+			colorMap.put(ChatColor.WHITE, new ColorSet<Integer, Integer, Integer>(255, 255, 255));
+		}
+	
+		private static class ColorSet<R, G, B> {
+			R red = null;
+			G green = null;
+			B blue = null;
+	
+			ColorSet(R red, G green, B blue) {
+				this.red = red;
+				this.green = green;
+				this.blue = blue;
+			}
+	
+			public R getRed() {
+				return red;
+			}
+	
+			public G getGreen() {
+				return green;
+			}
+	
+			public B getBlue() {
+				return blue;
+			}
+	
+		}
+	
+		public static ChatColor fromRGB(int r, int g, int b) {
+			TreeMap<Integer, ChatColor> closest = new TreeMap<Integer, ChatColor>();
+			colorMap.forEach((color, set) -> {
+				int red = Math.abs(r - set.getRed());
+				int green = Math.abs(g - set.getGreen());
+				int blue = Math.abs(b - set.getBlue());
+				closest.put(red + green + blue, color);
+			});
+			return closest.firstEntry().getValue();
+		}
+	
+	}
+
+	public static ChatColor fromRGB(int r, int g, int b) {
+		return ColorUtil.fromRGB(r, g, b);
+	}
+
 	public static class APIPlayer {
 		
 		public String name;
