d02cbdcb36224d5afd790c0c6dac355c315608e5
GamerCoder215 <gamercoder215@gmail.com>
2022-03-13 12:11:47 -0500

    Update BlockInventories.java
    - Scratched Anvil Inventory
    - Finish Enchanting Inventory

--- src/us/teaminceptus/noobysmp/inventory/BlockInventories.java
+++ src/us/teaminceptus/noobysmp/inventory/BlockInventories.java
@@ -14,21 +14,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import com.google.common.collect.ImmutableMap;
 
 import us.teaminceptus.noobysmp.SMP;
-import us.teaminceptus.noobysmp.materials.SMPMaterial;
-import us.teaminceptus.noobysmp.recipes.SMPRecipe;
-import us.teaminceptus.noobysmp.recipes.SMPRecipe.AnvilData;
 import us.teaminceptus.noobysmp.util.Generator;
 import us.teaminceptus.noobysmp.util.Items;
 import us.teaminceptus.noobysmp.util.PlayerConfig;
@@ -41,17 +36,6 @@ public class BlockInventories implements Listener {
 	public BlockInventories(SMP plugin) {
 		this.plugin = plugin;
 		Bukkit.getPluginManager().registerEvents(this, plugin);
-	}
-	
-	public static final Inventory getAnvilInventory() {
-		Inventory inv = Generator.genGUI(27, "Anvil", new BlockHolder(Material.ANVIL));
-		
-		inv.setItem(11, Items.Inventory.GUI_PANE);
-		for (int i = 13; i < 16; i++) inv.setItem(i, Items.Inventory.GUI_PANE);
-		
-		inv.setItem(14, new ItemStack(Material.ANVIL));
-		
-		return inv;
 	}
 	
 	public static final Inventory getEnchantingTable(Player p) {
@@ -83,9 +67,6 @@ public class BlockInventories implements Listener {
 	}
 	
 	private static final Map<Material, Inventory> inventoryMap = ImmutableMap.<Material, Inventory>builder()
-			.put(Material.ANVIL, getAnvilInventory())
-			.put(Material.CHIPPED_ANVIL, getAnvilInventory())
-			.put(Material.DAMAGED_ANVIL, getAnvilInventory())
 			.build();
 	
 	@EventHandler
@@ -109,63 +90,6 @@ public class BlockInventories implements Listener {
 			
 			inv.setResult(baseResult);
 			return;
-		}
-	}
-	
-	// RAW slots, use spigot for raw slot numbers
-	private static Map<Material, List<Integer>> noCancel = ImmutableMap.<Material, List<Integer>>builder()
-			.put(Material.ANVIL, Arrays.asList(10, 12, 16))
-			.put(Material.ENCHANTING_TABLE, Arrays.asList(13))
-			.build();
-	
-	@EventHandler
-	public void onClick(InventoryClickEvent e) {
-		InventoryView view = e.getView();
-		Inventory inv = view.getTopInventory();
-		if (!(e.getWhoClicked() instanceof Player p)) return;
-		PlayerConfig config = new PlayerConfig(p);
-		if (inv.getHolder() == null) return;
-		
-		if (inv.getHolder() instanceof BlockHolder holder) {
-			if (!(noCancel.get(holder.getMaterial()).contains(e.getRawSlot()))) e.setCancelled(true);
-			
-			if (holder.getMaterial() == Material.ANVIL) {
-				// Take Item
-				ItemStack item = e.getCurrentItem();
-				if (e.getRawSlot() == 16 && item != null && !Items.compareLocalization(item, Items.LOCKED_ITEM)) {
-					if (SMPRecipe.getAnvilRecipes().get(Items.getLocalization(item)) != null) {
-						AnvilData data = SMPRecipe.getAnvilRecipes().get(Items.getLocalization(item));
-						
-						inv.removeItem(data.getInput(), data.getResult());
-					}
-				}
-				
-				// Other Inventory Handles
-				if (inv.getItem(10) == null || inv.getItem(12) == null) {
-					inv.setItem(16, null);
-				}
-				
-				// Generate Recipe
-				if (inv.getItem(10) != null && inv.getItem(12) != null) {
-					for (AnvilData data : SMPRecipe.getAnvilRecipes().values()) {
-						if (Items.compareLocalization(inv.getItem(10), data.getInput()) && Items.compareLocalization(inv.getItem(12), data.getCombination())) {
-							// Check Level
-							String outputL = data.getResult().getItemMeta().getLocalizedName();
-							
-							if (SMPMaterial.getByLocalization(outputL) != null) {
-								SMPMaterial m = SMPMaterial.getByLocalization(outputL);
-								
-								if (m.getLevelUnlocked() < config.getLevel()) {
-									inv.setItem(16, Items.LOCKED_ITEM);
-								} else inv.setItem(16, m.getItem());
-								break;
-							}
-						}
-					}
-				}
-				
-			}
-			
 		}
 	}
 	
