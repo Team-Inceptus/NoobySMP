002471549150880d7a0ebbadda7778b60e793033
GamerCoder215 <gamercoder215@gmail.com>
2022-03-18 20:41:46 -0500

    Update BlazeKing.java

--- src/us/teaminceptus/noobysmp/entities/bosses/BlazeKing.java
+++ src/us/teaminceptus/noobysmp/entities/bosses/BlazeKing.java
@@ -1,18 +1,15 @@
 package us.teaminceptus.noobysmp.entities.bosses;
 
-import com.google.common.collect.ImmutableMap;
-
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
-import org.bukkit.Sound;
 import org.bukkit.entity.Blaze;
-import org.bukkit.entity.DragonFireball;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.SmallFireball;
 import org.bukkit.inventory.ItemStack;
+
+import com.google.common.collect.ImmutableMap;
 
 import us.teaminceptus.noobysmp.ability.cosmetics.SMPCosmetic;
 import us.teaminceptus.noobysmp.entities.bosses.BossSetup.Description;
@@ -51,35 +48,15 @@ public class BlazeKing extends SMPBoss<Blaze> {
         .build(), attributes(10000));
     }
 
-    @Repeated(1)
+    @Repeated(2)
     public void drawRing() {
-        if (entity.getHealth() > 3000)
-        SMPCosmetic.createCircle(Particle.FLAME, 2).accept(entity.getLocation().add(0, 0.5, 0));
-        else SMPCosmetic.createCircle(Particle.SOUL_FIRE_FLAME, 2).accept(entity.getLocation().add(0, 0.5, 0));
-    }
-
-    @Repeated(10)
-    public void superfireball() {
-        if (!(entity.getHealth() <= 3000)) return;
-
-        Location loc = entity.getEyeLocation();
-
-        SmallFireball f1 = entity.getWorld().spawn(loc, SmallFireball.class);
-        f1.setRotation(loc.getYaw(), loc.getPitch());
-
-        SmallFireball f2 = entity.getWorld().spawn(loc, SmallFireball.class);
-        f2.setRotation(loc.getYaw() + 90, loc.getPitch());
-
-        SmallFireball f3 = entity.getWorld().spawn(loc, SmallFireball.class);
-        f3.setRotation(loc.getYaw() + 180, loc.getPitch());
-
-        SmallFireball f4 = entity.getWorld().spawn(loc, SmallFireball.class);
-        f4.setRotation(loc.getYaw() + 270, loc.getPitch());
-
-        if (r.nextInt(100) < 3) {
-            DragonFireball f5 = entity.getWorld().spawn(loc, DragonFireball.class);
-            f5.setDirection(loc.getDirection());
-            entity.getWorld().playSound(entity, Sound.ENTITY_ENDER_DRAGON_GROWL, 6F, 1F);
+        if (entity.getHealth() > 3000) {
+        	SMPCosmetic.createCircle(Particle.FLAME, 2).accept(entity.getLocation().add(0, 0.5, 0));
+        	return;
+        }
+        else {
+        	SMPCosmetic.createCircle(Particle.SOUL_FIRE_FLAME, 2).accept(entity.getLocation().add(0, 0.5, 0));
+        	return;
         }
     }
     
